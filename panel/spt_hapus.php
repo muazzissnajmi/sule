cac3b6b8ef5b41364e2cdfb705f0eb0dd9df1e0f
muazzissnajmi <macmini@MacMiniM4.local>
2026-03-05 13:55:37 +0700

    fix: perbaiki blank page saat hapus SPT - fix  tanpa quotes, fix wrong column di DELETE pengikut, tambah null safety

--- panel/spt_hapus.php
+++ panel/spt_hapus.php
@@ -1,22 +1,20 @@
 <?php include 'session.php' ?>
 <?php
 include "../koneksi/koneksi.php";
-$sql_ = "SELECT * FROM spt WHERE id_spt = '$_GET[id]'";
-$tampil_ = mysqli_query($koneksi, $sql_);     
-$data_ = mysqli_fetch_array($tampil_);
-$no_spt_ = $data_['no_spt'];
 
-$sql = "SELECT * FROM spd WHERE no_spt = '$no_spt_'";
-$tampil = mysqli_query($koneksi, $sql);     
-$data = mysqli_fetch_array($tampil);
-$no_spt = $data['no_spt'];
+$id = isset($_GET['id']) ? mysqli_real_escape_string($koneksi, $_GET['id']) : '';
 
+if ($id != '') {
+    // Ambil no_spt dari SPT
+    $sql_ = "SELECT * FROM spt WHERE id_spt = '$id'";
+    $tampil_ = mysqli_query($koneksi, $sql_);     
+    $data_ = mysqli_fetch_array($tampil_);
+    $no_spt_ = $data_['no_spt'] ?? '';
 
-$sql3 = mysqli_query($koneksi, "DELETE FROM spt WHERE id_spt = '$_GET[id]'");
-$sql4 = mysqli_query($koneksi, "DELETE FROM pengikut WHERE no_spt = '$_GET[id]'");			
+    // Hapus data terkait
+    $sql3 = mysqli_query($koneksi, "DELETE FROM spt WHERE id_spt = '$id'");
+    $sql4 = mysqli_query($koneksi, "DELETE FROM pengikut WHERE no_spt = '$no_spt_'");
+}
 
-	
 echo "<script type='text/javascript'> document.location = '?page=spt&msg=1'; </script>";
-
-
 ?>
